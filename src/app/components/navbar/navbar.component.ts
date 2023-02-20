1ac8dd5ccb077bf25fb515012191e947c537ceda
Gangarig <112808191+Gangarig@users.noreply.github.com>
2023-02-20 03:36:42 +0100

    design changed

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,23 +1,27 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Directive, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.scss']
 })
+
 export class NavbarComponent implements OnInit {
 
   constructor() { }
 
   ngOnInit(): void {
   }
-  english(){
-
-  }
-  german(){
-
-  }
-  mongolian(){
-    
-  }
+    showNavLinks = false;
+    showMobile = false;
+    iconClass = 'bi-list';
+    toggle() {
+      this.showNavLinks = !this.showNavLinks;
+      this.showMobile = !this.showMobile;
+      if (this.iconClass === 'bi-list') {
+        this.iconClass = 'bi-x-lg';
+      } else {
+        this.iconClass = 'bi-list';
+      }
+    }
 }
